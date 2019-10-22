4fdebd1802ab0264b3c9167aeed4a28790aabe5e
srinivas <srinivas.nalla@imaginea.com>
2019-10-22 14:42:17 +0530

    Comments and db details updated

--- profile-service/src/main/java/com/pramati/crs/config/SweggerConfig.java
+++ profile-service/src/main/java/com/pramati/crs/config/SweggerConfig.java
@@ -10,7 +10,12 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+/**
+ * 
+ * @author srinivasn
+ *  
+ *  Enables the swaggers api
+ */
 @Configurable
 @EnableSwagger2
 public class SweggerConfig {
