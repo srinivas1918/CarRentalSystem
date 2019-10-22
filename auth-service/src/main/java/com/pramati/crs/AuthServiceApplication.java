5be302bbeef704238bb9f46c7803abcc0743437e
manikanthkumar <55873866+manikanthkumar@users.noreply.github.com>
2019-10-22 16:01:40 +0530

    Dev mani (#7)
    * Updated auth-service
    
    * Adding few configs
    
    * Correcting the package name for Docket

--- auth-service/src/main/java/com/pramati/crs/AuthServiceApplication.java
+++ auth-service/src/main/java/com/pramati/crs/AuthServiceApplication.java
@@ -24,8 +24,7 @@ public class AuthServiceApplication {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.basePackage("com.pramati.crs.authservice")).paths(PathSelectors.any())
-				.build();
+				.apis(RequestHandlerSelectors.basePackage("com.pramati.crs")).paths(PathSelectors.any()).build();
 	}
 
 	private ApiInfo apiInfo() {
